ae09cba389b797f0c3c7fc715a64bb0474cd8a2d
masutech16 <leafclover64@gmail.com>
2019-07-02 18:37:18 +0900

    implement drawing boards

--- src/gameView.ts
+++ src/gameView.ts
@@ -2,11 +2,13 @@ import { Renderer } from './renderer';
 import * as Color from './color'
 
 export class GameView {
+  cellSize: number
   stoneSize: number
   lineWidth: number
   renderer: Renderer
   constructor(ctx: CanvasRenderingContext2D, public width: number, public height: number) {
-    this.stoneSize = 64
+    this.cellSize = 64
+    this.stoneSize = 60
     this.lineWidth = 2
     this.renderer = new Renderer(ctx)
   }
@@ -15,15 +17,30 @@ export class GameView {
     this.renderer.drawRect(0, 0, this.width, this.height, Color.green)
     // 縦線と横線を引く
     for (let i = 0; i < 9; i++) {
-      const interval = this.stoneSize + this.lineWidth
+      const interval = this.cellSize + this.lineWidth
       const offset = 1 // 0始まりだと一番上の線が欠けるため
       this.renderer.drawLine(0, interval * i + offset, this.width, interval * i + offset, this.lineWidth, Color.black)
       this.renderer.drawLine(interval * i + offset, 0, interval * i + offset, this.height, this.lineWidth, Color.black)
     }
   }
 
-  drawStone(board: Array<Array<number>>) {
+  drawStones(board: Array<Array<Stone>>) {
+    for (let y = 0; y < board.length; y++) {
+      for (let x = 0; x < board[y].length; x++) {
+        if (board[y][x] == 0) continue
+        const c = board[y][x] == 1 ? Color.black : Color.white
+        this.drawStone(x, y, c);
+      }
+    }
+  }
 
+  drawStone(x: number, y: number, color: Color) {
+    if (x <= 0 || 9 <= x) return
+    if (y <= 0 || 9 <= y) return
+    const rx = x * this.lineWidth + (x - 1) * this.cellSize + this.cellSize / 2;
+    const ry = y * this.lineWidth + (y - 1) * this.cellSize + this.cellSize / 2;
+
+    this.renderer.drawCircle(rx, ry, this.stoneSize / 2, color)
   }
 
   clearScreen() {
--- src/index.d.ts
+++ src/index.d.ts
@@ -3,3 +3,6 @@ type White = 'rgb(255,255,255)'
 type Green = 'rgb(0,126,78)'
 type Black = 'rgb(0,0,0)'
 type Color = White | Green | Black
+
+// Stone empty - black -white
+type Stone = 0 | 1 | 2
